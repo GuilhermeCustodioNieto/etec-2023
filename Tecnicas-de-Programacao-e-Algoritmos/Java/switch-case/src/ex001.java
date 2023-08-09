5b05e067effb6822cc93603c53b15284d28fe05f
Guilherme Custodio Nieto <guilhermecustodionieto@gmail.com>
2023-08-09 00:00:28 -0300

    Exercicio 1 finalizado com sucesso

---
+++ Tecnicas-de-Programacao-e-Algoritmos/Java/switch-case/src/ex001.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class ex001 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int idadeAluno = scan.nextInt();
+		
+		switch (idadeAluno){
+			case 6:
+				System.out.println("CATEGORIA = dente de leite");
+				break;
+			case 7:
+				System.out.println("CATEGORIA = junior");
+				break;
+			case 8:
+				System.out.println("CATEGORIA = junior max");
+				break;
+			case 9:
+				System.out.println("CATEGORIA = junior master");
+				break;
+			case 10:
+				System.out.println("CATEGORIA = master");
+				break;
+			default:
+				System.out.println("essa idade nào é aceita!");
+			
+		}
+		
+		scan.close();
+	}
+
+}
